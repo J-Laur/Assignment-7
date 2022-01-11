748c056ffd4f623676be46c976c83371de1c0363
J-Laur <jessica.lauria@csi.cuny.edu>
2022-01-11 14:32:02 -0500

    Working on utilizing API key

---
+++ img-search/src/App.js
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import CoffeeCard from './components/CoffeeCard';
+
+function App() {
+
+  const [data, setData] = useState([])
+
+  let url = `https://api.sampleapis.com/coffee/hot`
+  let API_KEY = "vlBltm1oSgXB2gSk1qXFa39jxmt6UXAx"
+
+  const getData = async () => {
+    await axios.get(url)
+      .then(res => {
+        setData(res.data)
+      })
+  }
+
+  useEffect(() => {
+    getData()
+  }, [])
+
+  return (
+    <div>
+      <div className="p-5 bg-dark fs-1">
+        <div className="text-light text-center"><b>API</b></div>
+      </div>
+
+      <div className="container d-flex justify-content-center">
+        <div className="m-3">APIs</div>
+      </div>
+
+      <div className="container">
+        {
+          data.map(element => (
+            < div key={element.id} >
+              <CoffeeCard coffee={element} />
+            </div>
+          )
+          )
+        }
+      </div>
+    </div >
+
+  )
+}
+
+export default App;
